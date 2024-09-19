f96efca1dfe839382d53797e28f42ec3ed7da720
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-19 13:34:23 +0200

    Add StringUtility class with string manipulation methods
    This commit introduces a new StringUtility class containing utility methods for string manipulation such as replacing the last occurrence of a regex, reversing a string, replacing the first occurrence of a string, and truncating a string to a specified length with ellipsis. These methods aim to streamline common string operations in the codebase.

---
+++ commom/src/main/java/dev/luan/javautility/common/StringUtility.java
@@ -0,0 +1,49 @@
+package dev.luan.javautility.common;
+
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * This class adds some utility functions for strings
+ */
+@UtilityClass
+public class StringUtility {
+
+    /**
+     * Replaces the last seen `regex` string in the string with
+     * the `replacement`
+     *
+     * @param s           the string which will be modified
+     * @param regex       the regex to match
+     * @param replacement the replacement
+     * @return the modified string
+     */
+    public String replaceLast(@NotNull String s, String regex, String replacement) {
+        return s.replaceFirst("(?s)(.*)" + regex, "$1" + replacement);
+    }
+
+    /**
+     * Reverses the given string
+     *
+     * @param s the string to reverse
+     * @return the reversed string
+     */
+    public @NotNull String reverse(@NotNull String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    public String replaceFirst(String base, String matcher, String replacement) {
+        return base.replaceFirst(Pattern.quote(matcher), replacement);
+    }
+
+    public String capAtNCharacters(@NotNull String toCap, int n) {
+        if (toCap.length() <= n) return toCap;
+        String s = toCap.substring(0, n - 3);
+        return s + "...";
+    }
+
+}
