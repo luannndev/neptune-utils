bc0b36d6267dbccde67b0b1f2e93e55449804c1d
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-19 13:40:06 +0200

    Add unit tests for StringUtility class
    Created tests for reverse, replaceLast, and capAtNCharacters methods in the StringUtility class. These tests use the AssertJ library to validate expected behavior of each method. This ensures robustness and reliability of the string utility functions.

---
+++ commom/src/test/java/dev/luan/javautility/common/StringUtilityTest.java
@@ -0,0 +1,30 @@
+package dev.luan.javautility.common;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringUtilityTest {
+
+    @Test
+    public void testReverse() {
+        assertThat(StringUtility.reverse("123")).isEqualTo("321");
+        assertThat(StringUtility.reverse("abc cde")).isEqualTo("edc cba");
+    }
+
+    @Test
+    public void testReplaceLast() {
+        assertThat(StringUtility.replaceLast("Test Test", "Test", "1")).isEqualTo("Test 1");
+        assertThat(StringUtility.replaceLast("Test", "Test", "")).isEqualTo("");
+        assertThat(StringUtility.replaceLast("1 2 3", "[0-9] [0-9]", "")).isEqualTo("1 ");
+        assertThat(StringUtility.replaceLast("1 2 3", "[0-9]", "")).isEqualTo("1 2 ");
+    }
+
+    @Test
+    public void testCap() {
+        assertThat(StringUtility.capAtNCharacters("12345", 3)).isEqualTo("...");
+        assertThat(StringUtility.capAtNCharacters("12345678", 6)).isEqualTo("123...");
+    }
+
+}
