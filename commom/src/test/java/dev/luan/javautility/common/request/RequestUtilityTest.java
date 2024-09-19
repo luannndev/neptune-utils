c1c7f3fdc6cdfd77c9ef29f1e3a845ff855c330a
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-19 13:36:30 +0200

    Add unit tests for RequestUtility and Response class
    Introduced `RequestUtilityTest` to validate `getJsonResponse` and `buildUriWithParams` methods. Also created a `Response` class with Jackson annotations to support JSON deserialization in integration tests.

---
+++ commom/src/test/java/dev/luan/javautility/common/request/RequestUtilityTest.java
@@ -0,0 +1,44 @@
+package dev.luan.javautility.common.request;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestUtilityTest {
+
+    @Test
+    public void testGetResponse() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            HttpResponse<String> response = RequestUtility
+                    .getJsonResponse(
+                            HttpRequest.newBuilder()
+                                    .uri(new URI("https://catfact.ninja/fact"))
+                                    .GET()
+                    );
+            String body = response.body();
+            assertThat(body).isNotNull();
+            assertThat(mapper.readValue(body, Response.class)).isNotNull();
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testBuildURIWithParams() throws URISyntaxException {
+        URI uri = RequestUtility.buildUriWithParams("http://localhost:1080/testParams", Map.of("key1", "value1", "key2", "value2"));
+        assertThat(uri.toString()).isIn("http://localhost:1080/testParams?key1=value1&key2=value2", "http://localhost:1080/testParams?key2=value2&key1=value1");
+    }
+
+
+
+}
---
+++ commom/src/test/java/dev/luan/javautility/common/request/Response.java
@@ -0,0 +1,19 @@
+package dev.luan.javautility.common.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Jacksonized
+@Data
+public class Response {
+
+    private String fact;
+    private int length;
+
+}
