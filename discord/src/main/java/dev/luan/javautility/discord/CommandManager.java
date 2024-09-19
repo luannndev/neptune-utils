ca9a46c38c1d4abe775ea89b5171f8d826f34e2d
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-19 15:54:03 +0200

    Add command registration and message handling in CommandManager
    This commit introduces the ability to register and handle commands in the CommandManager class. The onMessageReceived method processes commands with predefined prefixes and calls a default method if the command is not found or an error occurs. Also included is the functionality to register commands through the registerCommand method, enhancing the overall command management system.

--- discord/src/main/java/dev/luan/javautility/discord/CommandManager.java
+++ discord/src/main/java/dev/luan/javautility/discord/CommandManager.java
@@ -1,13 +1,83 @@
 package dev.luan.javautility.discord;
 
+import dev.luan.javautility.discord.exception.CommandNotFoundException;
+import dev.luan.javautility.discord.parsed.ParsedCommand;
+import dev.luan.javautility.discord.reciever.ReceiveContext;
+import dev.luan.javautility.discord.reciever.predefined.PrefixedReceiver;
+import lombok.Getter;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.*;
 
 public class CommandManager extends ListenerAdapter {
 
     private final JDA jda;
 
+    @Getter
+    private final Set<ParsedCommand> commands = new HashSet<>();
+
+
     public CommandManager(JDA jda) {
         this.jda = jda;
+        jda.addEventListener(this);
     }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        for (ParsedCommand command : this.commands) {
+            if (command.getReceiver() instanceof PrefixedReceiver prefixed) {
+                try {
+                    if (!prefixed.receive(event)) {
+                        defaultMethodCall(event, command, new CommandNotFoundException("Command was not found"), prefixed.getArgs());
+                    }
+                } catch (Throwable e) {
+                    defaultMethodCall(event, command, e, new ArrayList<>());
+                }
+
+            }
+
+        }
+    }
+
+    private static void defaultMethodCall(MessageReceivedEvent event, @NotNull ParsedCommand command, Throwable e, List<Object> args) {
+        try {
+            Optional<Method> defaultCommand = Arrays.stream(command.getCommand().getClass().getDeclaredMethods())
+                    .filter(method -> method.getName().equals("defaultCommand"))
+                    .findAny();
+            if (defaultCommand.isEmpty()) {
+                throw new RuntimeException(e);
+            }
+            defaultCommand.get().setAccessible(true);
+            List<Object> params = new ArrayList<>();
+            for (Parameter parameter : defaultCommand.get().getParameters()) {
+                if (parameter.getType().isAssignableFrom(Throwable.class)) {
+                    params.add(e);
+                    continue;
+                }
+                if (parameter.getType().isAssignableFrom(ReceiveContext.class)) {
+                    params.add(new ReceiveContext(command, args));
+                    continue;
+                }
+                if (parameter.getType().isAssignableFrom(MessageReceivedEvent.class)) {
+                    params.add(event);
+                    continue;
+                }
+                params.add(null);
+            }
+            defaultCommand.get().invoke(command.getCommand(), params.toArray(Object[]::new));
+        } catch (InvocationTargetException | IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void registerCommand(@NotNull Object command) {
+        this.commands.add(new ParsedCommand(command, this));
+    }
+
 }
