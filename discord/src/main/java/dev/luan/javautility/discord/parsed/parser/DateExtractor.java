c2e9b854624448b61705ce59395c3b0b123bcad1
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-19 15:54:14 +0200

    Add DateExtractor to parse dates from strings
    Introduced a new DateExtractor class extending Extractor to parse dates dynamically from provided strings. This class iterates through the string, attempting to parse dates and returns the latest parsed date as a Pair.

---
+++ discord/src/main/java/dev/luan/javautility/discord/parsed/parser/DateExtractor.java
@@ -0,0 +1,29 @@
+package dev.luan.javautility.discord.parsed.parser;
+
+import dev.luan.javautility.common.DateUtility;
+import kotlin.Pair;
+
+import java.util.Date;
+
+public class DateExtractor extends Extractor<Pair<String, Date>> {
+    @Override
+    public Pair<String, Date> extract(String s) {
+        StringBuilder current = new StringBuilder();
+        Pair<String, Date> latest = null;
+
+        for (int i = 0; i < s.length(); i++) {
+            try {
+                Pair<String, Date> tmp = new Pair<>(current.toString(), DateUtility.parseDynamic(current.toString()));
+
+                if (latest != null && latest.getSecond().getTime() == tmp.getSecond().getTime() && !latest.getFirst().equals(tmp.getFirst())) {
+                    return tmp;
+                }
+                latest = tmp;
+            } catch (Exception ignored) {}
+            current.append(s.charAt(i));
+        }
+
+        return latest;
+    }
+
+}
